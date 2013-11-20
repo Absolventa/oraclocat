e6144a25e6f2d2b692d0ce2d1370564fd7d617a5
Robin Neumann <robin.neumann@absolventa.de>
2013-11-20 10:49:23 +0100

    add developers for deployment

---
+++ developers.rb
@@ -0,0 +1,8 @@
+DEVELOPERS = {
+  "Daniel"  => { github: "@dansch"       },
+  "Carsten" => { github: "@carpodaster"  },
+  "Alex"    => { github: "@agieche"      },
+  "Robin"   => { github: "@neumanrq"     },
+  "Felix"   => { github: "@felixmohnert" },
+  "Markus" =>  { github: "@axlwaii"      }
+}
